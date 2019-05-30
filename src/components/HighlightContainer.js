296c659aed5e5e93128b1033f302ceffbb211736
Robert Logan <rloganiv@gmail.com>
2019-05-30 14:46:56 -0700

    Explore functionality added

---
+++ src/components/HighlightContainer.js
@@ -0,0 +1,26 @@
+// Source: https://github.com/allenai/allennlp-demo
+
+import React from 'react';
+import '../css/HighlightContainer.css';
+
+export default class HighlightContainer extends React.Component {
+  render() {
+    const {         // All fields optional:
+      children,       // object | string
+      isClicking,     // boolean
+      layout          // string (supported values: "bottom-labels", null)
+    } = this.props;
+
+    const containerClasses = `passage
+      model__content__summary
+      highlight-container
+      ${layout ? "highlight-container--" + layout : ""}
+      ${isClicking ? "clicking" : ""}`;
+
+    return (
+      <div className={containerClasses}>
+        {children}
+      </div>
+    );
+  }
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Row from 'react-bootstrap/Row';
+import { Link } from 'react-router-dom';
 
-import '../css/home.css';
+import '../css/Home.css';
 
 
 const bibtex = `@inproceedings{rlogan2019kglm,
@@ -24,75 +25,89 @@ const bibtex = `@inproceedings{rlogan2019kglm,
 }`
 
 
+class Footer extends React.Component {
+    render () {
+        return (
+            <footer className="container">
+                <hr/>
+                <p>
+                    This site is created and maintained by&nbsp;
+                    <a href="https://rloganiv.github.io">rloganiv</a>.
+                </p>
+            </footer>
+        )
+    }
+}
+
+
 class Home extends React.Component {
     render () {
         return (
-            <main role="main">
-                <Jumbotron>
-                    <Container>
-                        <h1 class="display-3">
-                            Linked WikiText-2
-                        </h1>
-                        <p>
-                            A nice description of the Linked WikiText-2 dataset.
-                        </p>
-                        <p>
+            <Container>
+                <main role="main">
+                    <Jumbotron>
+                        <Container>
+                            <h1 className="display-3">
+                                Linked WikiText-2
+                            </h1>
+                            <p>
+                                A nice description of the Linked WikiText-2 dataset.
+                            </p>
                             <ButtonToolbar>
-                                <Button href="#" variant="primary" size="lg">Download</Button>
-                                <Button variant="primary" size="lg">Explore</Button>
+                                <Link className="btn btn-primary btn-lg" to="/" variant="primary" size="lg">Download</Link>
+                                <Link className="btn btn-primary btn-lg" to="/explore" variant="primary" size="lg">Explore</Link>
                             </ButtonToolbar>
-                        </p>
+                        </Container>
+                    </Jumbotron>
+                    <Container>
+                        <Row>
+                            <Col md={4}>
+                                <h2>Acknowledgements</h2>
+                                <p>
+                                We would like to thank the following people for making this work possible:
+                                </p>
+                                <p>
+                                    <a href="https://smerity.com">Stephen Merity</a> for sharing the scripts used to collect the Wikitext-2 dataset.
+                                </p>
+                                <p>
+                                    <a href="https://">Nitish Gupta</a> for modifying his <a href="https://github.com/nitishgupta/neural-el">neural-el</a> code to easily run on our data.
+                                </p>
+                                <p>
+                                    The numerous contributors that have made <a href="https://www.wikipedia.org">Wikipedia</a> and <a href="https://www.wikidata.org">Wikidata</a> such fantastic repositories of knowledge.
+                                </p>
+                            </Col>
+                            <Col md={4}>
+                                <h2>Citation</h2>
+                                <p>
+                                If you use this dataset in your work please cite:
+                                </p>
+                                <pre>
+                                <code>
+                                    { bibtex }
+                                </code>
+                                </pre>
+                            </Col>
+                            <Col md={4}>
+                                <h2>License</h2>
+                                <p>
+                                The Linked WikiText-2 Dataset is distributed under a&nbsp;
+                                <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/us/">CC-BY-SA-3.0</a> license.
+                                </p>
+                                <p>
+                                The Linked WikiText-2 Dataset is derivative work of Saleforce Research's &nbsp;
+                                <a href="https://blog.einstein.ai/the-wikitext-long-term-dependency-language-modeling-dataset/">WikiText-2 Dataset</a>
+                                &nbsp;
+                                which is distributed under the same license.
+                                </p>
+                                <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/us/">
+                                <img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-sa/3.0/us/88x31.png" />
+                                </a>
+                            </Col>
+                        </Row>
                     </Container>
-                </Jumbotron>
-                <Container>
-                    <Row>
-                        <Col md='4'>
-                            <h2>Acknowledgements</h2>
-                            <p>
-                            We would like to thank the following people for making this work possible:
-                            </p>
-                            <p>
-                                <a href="https://smerity.com">Stephen Merity</a> for sharing the scripts used to collect the Wikitext-2 dataset.
-                            </p>
-                            <p>
-                                <a href="https://">Nitish Gupta</a> for modifying his <a href="https://github.com/nitishgupta/neural-el">neural-el</a> code to easily run on our data.
-                            </p>
-                            <p>
-                                The numerous contributors that have made <a href="https://www.wikipedia.org">Wikipedia</a> and <a href="https://www.wikidata.org">Wikidata</a> such fantastic repositories of knowledge.
-                            </p>
-                        </Col>
-                        <Col md='4'>
-                            <h2>License</h2>
-                            <p>
-                            The Linked WikiText-2 Dataset is distributed under a&nbsp;
-                            <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/us/">CC-BY-SA-3.0</a> license.
-                            </p>
-                            <p>
-                            The Linked WikiText-2 Dataset is derivative work of Saleforce Research's &nbsp;
-                            <a href="https://blog.einstein.ai/the-wikitext-long-term-dependency-language-modeling-dataset/">WikiText-2 Dataset</a>
-                            &nbsp;
-                            which is distributed under the same license.
-                            </p>
-                            <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/us/">
-                            <img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-sa/3.0/us/88x31.png" />
-                            </a>
-                        </Col>
-                        <Col md='4'>
-                            <h2>Citation</h2>
-                            <p>
-                            If you use this dataset in your work please cite:
-                            </p>
-                            <pre>
-                            <code>
-                                { bibtex }
-                            </code>
-                            </pre>
-                        </Col>
-
-                    </Row>
-
-                </Container>
-            </main>
+                <Footer/>
+                </main>
+            </Container>
         )
     }
 }
