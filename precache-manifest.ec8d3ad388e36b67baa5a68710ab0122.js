c9476a680abd8ab14fb74bb94486af1179df7f38
Robert L. Logan IV <rloganiv@gmail.com>
2019-05-29 22:29:55 -0700

    Updates

---
+++ precache-manifest.ec8d3ad388e36b67baa5a68710ab0122.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fac4387221a3fd21cad43c933c3fe6",
+    "url": "/linked-wikitext-2/index.html"
+  },
+  {
+    "revision": "bf81e4b1b942d30fa076",
+    "url": "/linked-wikitext-2/static/css/main.a050e7ca.chunk.css"
+  },
+  {
+    "revision": "c2df02cc97627f3a4670",
+    "url": "/linked-wikitext-2/static/js/2.ef698cd4.chunk.js"
+  },
+  {
+    "revision": "bf81e4b1b942d30fa076",
+    "url": "/linked-wikitext-2/static/js/main.b1684f44.chunk.js"
+  },
+  {
+    "revision": "f0db0ad5764b24155768",
+    "url": "/linked-wikitext-2/static/js/runtime~main.43b40afa.js"
+  }
+]);
